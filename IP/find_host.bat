4abf3d7bb8c343ffa5c131f8fbb78330d56b0a6b
yiyah <756307810@qq.com>
2021-07-25 16:20:37 +0800

    show adapter at begin

--- IP/find_host.bat
+++ IP/find_host.bat
@@ -7,7 +7,7 @@ echo note: just support netmask 255.255.255.0
 echo scan time about 2 minutes (4 hosts)
 echo.
 echo Found the following IP in your system. Maybe helpful for you.
-ipconfig | findstr IPv4
+call :showMyAdapter
 echo.
 
 set networkSegment=192.168.1.0
@@ -45,3 +45,23 @@ echo Total time is %totalTime_minute% min %totalTime_second% sec.
 
 :failed
 pause >nul
+
+
+:showMyAdapter
+setlocal enabledelayedexpansion
+for /f "delims=:  tokens=1,3" %%i in ('ipconfig ^| findstr /n IPv4') do (
+    rem get the IP and the row where it is
+    set /a IP_Line=%%i
+    set IP_origin=%%j
+
+    rem get the row where the adapter is
+    set /a adapter_line=!IP_Line!-4
+    rem remove the blank in front of IP
+    set IP=!IP_origin:~1,15!
+
+    rem show after finding the row where the adapter is
+    for /f "tokens=1* delims=:" %%a in ('ipconfig^|findstr /n .*') do (
+        if %%a equ !adapter_line! echo %%b & echo !IP! & echo.
+    )
+)
+goto :eof
