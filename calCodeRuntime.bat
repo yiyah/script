c2318d713f96836c0bac7d97387a3e1e05490fe8
yiyah <756307810@qq.com>
2021-07-29 23:02:45 +0800

    :bug: Fix: Fix #1 calculate code runtime error
    [Description]
    When the value is 08 or 09, the batch program will think it is
    octonary.The number 8 and 9 exceed the octal range, so an error
    occurred.

--- calCodeRuntime.bat
+++ calCodeRuntime.bat
@@ -3,17 +3,27 @@ title Calculate the program running ^time
 
 set time_begin=%time%
 
-:: Add the code which need detect in here
-:: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-:: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+:: Add code in the area below
+
+ping 127.0.0.1
+
+:: Add code in the area above
 
 set time_end=%time%
 
+:: get begin time minute and second
+if %time_begin:~3,1% == 0 (set /a time_begin_min=%time_begin:~4,1%) else (set /a time_begin_min=%time_begin:~3,2%)
+if %time_begin:~-5,1% == 0 (set /a time_begin_second=%time_begin:~-4,1%) else (set /a time_begin_second=%time_begin:~-5,2%)
+
+:: get end time minute and second
+if %time_end:~3,1% == 0 (set /a time_end_min=%time_end:~4,1%) else (set /a time_end_min=%time_end:~3,2%)
+if %time_end:~-5,1% == 0 (set /a time_end_second=%time_end:~-4,1%) else (set /a time_end_second=%time_end:~-5,2%)
+
 :: get totalTime_minute
-if %time_end:~3,2% lss %time_begin:~3,2% (set /a totalTime_minute = %time_end:~3,2% + 60 - %time_begin:~3,2%) else set /a totalTime_minute = %time_end:~3,2% - %time_begin:~3,2%
+if %time_end_min% lss %time_begin_min% (set /a totalTime_minute = %time_end_min% + 60 - %time_begin_min%) else (set /a totalTime_minute = %time_end_min% - %time_begin_min%)
 
 :: get totalTime_second
-if %time_end:~-5,2% lss %time_begin:~-5,2% (set /a totalTime_second = %time_end:~-5,2% + 60 - %time_begin:~-5,2% & set /a totalTime_minute = %totalTime_minute% - 1) else set /a totalTime_second = %time_end:~-5,2% - %time_begin:~-5,2%
+if %time_end_second% lss %time_begin_second% (set /a totalTime_second = %time_end_second% + 60 - %time_begin_second% & set /a totalTime_minute = %totalTime_minute% - 1) else (set /a totalTime_second = %time_end_second% - %time_begin_second%)
 
 echo.
 echo Time info: %time_begin% -- %time_end%
