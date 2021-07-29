2d704dd48fce1e8096bf2605edae2410a0258dd7
yiyah <756307810@qq.com>
2021-07-29 23:17:16 +0800

    :bug: Fix: Fix calculate running time error #1
    [Description]
    Fix the bug of calculate running time error. issue #1

--- IP/find_host.bat
+++ IP/find_host.bat
@@ -33,11 +33,19 @@ for /l %%i in (1,1,254) do ping -w 2 -n 1 %i1%.%i2%.%i3%.%%i | find "TTL="
 
 set time_end=%time%
 
+:: get begin time minute and second
+if %time_begin:~3,1% == 0 (set /a time_begin_min=%time_begin:~4,1%) else (set /a time_begin_min=%time_begin:~3,2%)
+if %time_begin:~-5,1% == 0 (set /a time_begin_second=%time_begin:~-4,1%) else (set /a time_begin_second=%time_begin:~-5,2%)
+
+:: get end time minute and second
+if %time_end:~3,1% == 0 (set /a time_end_min=%time_end:~4,1%) else (set /a time_end_min=%time_end:~3,2%)
+if %time_end:~-5,1% == 0 (set /a time_end_second=%time_end:~-4,1%) else (set /a time_end_second=%time_end:~-5,2%)
+
 :: get totalTime_minute
-if %time_end:~3,2% lss %time_begin:~3,2% (set /a totalTime_minute = %time_end:~3,2% + 60 - %time_begin:~3,2%) else set /a totalTime_minute = %time_end:~3,2% - %time_begin:~3,2%
+if %time_end_min% lss %time_begin_min% (set /a totalTime_minute = %time_end_min% + 60 - %time_begin_min%) else (set /a totalTime_minute = %time_end_min% - %time_begin_min%)
 
 :: get totalTime_second
-if %time_end:~-5,2% lss %time_begin:~-5,2% (set /a totalTime_second = %time_end:~-5,2% + 60 - %time_begin:~-5,2% & set /a totalTime_minute = %totalTime_minute% - 1) else set /a totalTime_second = %time_end:~-5,2% - %time_begin:~-5,2%
+if %time_end_second% lss %time_begin_second% (set /a totalTime_second = %time_end_second% + 60 - %time_begin_second% & set /a totalTime_minute = %totalTime_minute% - 1) else (set /a totalTime_second = %time_end_second% - %time_begin_second%)
 
 echo.
 echo scan is finish!
